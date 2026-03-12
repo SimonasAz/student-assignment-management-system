468385881a4f6ee686cf042a058d99abb2e08f47
SimonasAz <simonasazguridis@gmail.com>
2026-03-12 22:02:43 +0200

    Created the register page by using shadcn package components for the UI

---
+++ app/register/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card"
+
+export default function Register() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+
+    async function handleSubmit(e: any) {
+        e.preventDefault()
+
+        await fetch("/api/register"), {
+            method: "POST",
+            body: JSON.stringify({ name, email, password})
+        }
+    }
+
+    return (
+    <div className="flex justify-center mt-20">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle>Create Account</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <form onSubmit={handleSubmit} className="space-y-4">
+                <Input placeholder="Name" onChange={(e)=>setName(e.target.value)} />
+                <Input placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
+                <Input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                <Button type="submit">Register</Button>
+            </form>
+        </CardContent>
+        </Card>
+        </div>
+    )
+}
