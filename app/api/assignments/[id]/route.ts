8bb8bb6ac52dc3fc652c27a5383dd6d09eb021ed
SimonasAz <simonasazguridis@gmail.com>
2026-03-14 12:46:26 +0200

    Added the backend logic for the edit button

--- app/api/assignments/[id]/route.ts
+++ app/api/assignments/[id]/route.ts
@@ -20,3 +20,30 @@ export async function DELETE(
 
     }
 }
+export async function PUT(
+    req: Request,
+    context: { params: Promise<{id: string}> }
+){
+    const { id } =  await context.params
+    const data = await req.json()
+
+    try{
+
+    const assignment =  await prisma.assignment.update({
+        where: {id},
+        data:{
+            title: data.title,
+            deadline: new Date(data.deadline),
+            status: data.status,
+            difficulty: parseInt(data.difficulty)
+        }
+    })
+    } catch {
+           return Response.json(
+            { error: "Assignment not found" },
+            { status: 404 }
+           )
+    }
+
+    
+}
