0cef24cdf0d04ad6609234fdfd95d7b58eed2ff2
SimonasAz <simonasazguridis@gmail.com>
2026-03-26 00:50:49 +0200

    Added category API routes

---
+++ app/api/categories/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/prisma/lib/prisma";
+import { getCurrentUser } from "@/lib/auth";
+
+export async function GET() {
+    const user = await getCurrentUser()
+    if (!user) {
+        return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 })
+    }
+
+    const categories = await prisma.category.findMany()
+
+    return Response.json(categories)
+}
+
+export async function POST(req: Request) {
+    const user = await getCurrentUser()
+    if (!user) {
+        return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 })
+    }
+
+    const data = await req.json()
+    if (!data.name) {
+        return new Response(JSON.stringify({ error: "Name is required" }), { status: 400 })
+    }
+    try{
+        const category = await prisma.category.create({
+            data: {
+                name: data.name
+            }
+        })
+        return Response.json(category)
+    } catch {
+        return Response.json({error: "Category already exists"}, { status: 400 })
+    }  
+
+}
