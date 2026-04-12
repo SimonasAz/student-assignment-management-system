96e2376326a2421f536bfde9b78c588fda3bf8f1
SimonasAz <simonasazguridis@gmail.com>
2026-04-13 00:57:45 +0300

    Added email checkign so the user can't create an email that already exists

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,22 +2,39 @@ import prisma from "@/prisma/lib/prisma"
 import bcrypt from "bcrypt"
 
 export async function POST(req: Request) {
-    const data = await req.json()
+    try{
 
-    const hashedPassword = await bcrypt.hash(data.password, 10)
+        const data = await req.json()
+        const email = data.email.toLowerCase().trim()
 
-    const user = await prisma.user.create({
-        data: {
-            name: data.name,
-            email: data.email,
-            password: hashedPassword
-            
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if(existingUser){
+            return Response.json({error: "User already exists"}, {status: 400})
         }
-        
-    })
-    return Response.json({
-        id: user.id,
-        name: user.name,
-        email: user.email
-    })
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+
+        const user = await prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                password: hashedPassword
+                
+            }
+            
+        })
+        return Response.json({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        })
+
+    }
+    catch(err){
+        return Response.json({error: "Something went wrong"}, {status: 500})
+    }
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -12,18 +12,28 @@ export default function Register() {
     const [password, setPassword] = useState("")
     const router = useRouter();
     const [name, setName] = useState("")
+    const [error, setError] = useState("");
+    
     
 
     async function handleSubmit(e: any) {
         e.preventDefault()
 
-        await fetch("/api/register", {
+        const res = await fetch("/api/register", {
             method: "POST",
             headers: {
             "Content-Type": "application/json"
         },
             body: JSON.stringify({ name, email, password})
         })
+        const data = await res.json()
+
+        if(!res.ok){
+            setError(data.error || "Something went wrong")
+            return
+        }
+
+        setError("")
         router.push("/login")
     }
     // Returning the UI
@@ -34,12 +44,19 @@ export default function Register() {
           <CardTitle>Create Account</CardTitle>
         </CardHeader>
         <CardContent>
+           
             <form onSubmit={handleSubmit} className="space-y-4">
                 <Input placeholder="Name" onChange={(e)=>setName(e.target.value)} />
                 <Input placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
                 <Input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                {error && (
+                <div className="bg-red-100 border border-red-300 text-red-700 px-2 py-2 rounded">
+                    {error}
+                </div>
+                )}
                 <Button type="submit" className="cursor-pointer">Register</Button>
             </form>
+             
         </CardContent>
         </Card>
         </div>
